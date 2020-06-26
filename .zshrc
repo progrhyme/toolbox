64d74abcfde02ffb9a856920385e2ed0811836f2
progrhyme <progrhyme@gmail.com>
2020-06-26 19:16:04 +0900

    Clean up unused materials

--- .zshrc
+++
@@ -1,1 +0,0 @@
-export PATH="$PATH:$HOME/gitrepos/tools/bin"
--- npm/prepare-hubot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pkgs=(hubot coffee-script yo generator-hubot)
-
-npm install -g ${pkgs[*]}
-
-exit
